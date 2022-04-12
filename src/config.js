f13f93742cd4aa7cc98216d3356e08bb25466a40
Lukas Gisder-Dubé <lukas@dube.io>
2022-04-12 22:29:10 +0200

    add session creation

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 3300,
-  IVY_API_KEY: process.env.IVY_API_KEY,
-  CDN_URL: process.env.CDN_URL || 'https://cdn.dev.getivy.de',
+  IVY_API_KEY: process.env.IVY_API_KEY || '',
+  IVY_CDN_URL: process.env.IVY_CDN_URL || 'https://cdn.dev.getivy.de',
+  IVY_API_URL: process.env.IVY_API_URL || 'https://api.dev.getivy.de/api',
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const router = express.Router()
 const cart = require('../cart.json')
 const config = require('../config')
@@ -21,7 +22,7 @@ router.get('/callback/error', (req, res) => {
   })
 })
 
-router.get('/', (req, res) => {
+function getCartPrice() {
   const subtotal = parseFloat(
     cart.items
       .reduce((acc, item) => {
@@ -30,14 +31,63 @@ router.get('/', (req, res) => {
       .toFixed(2)
   )
 
+  const subtotalNet = parseFloat(
+    cart.items
+      .reduce((acc, item) => {
+        return acc + item.price_net
+      }, 0)
+      .toFixed(2)
+  )
+
+  return {
+    subtotal,
+    subtotal_net: subtotalNet,
+    shipping: cart.shipping,
+    vat: subtotal - subtotalNet,
+    shipping: cart.shipping,
+    total: subtotal + cart.shipping,
+  }
+}
+
+router.get('/', (req, res) => {
   res.render('shop', {
     title: 'Checkout - Cart',
     items: cart.items,
-    subtotal,
-    shipping: cart.shipping,
-    total: subtotal + cart.shipping,
-    cdnUrl: config.CDN_URL,
+    ...getCartPrice(),
+    cdnUrl: config.IVY_CDN_URL,
   })
+})
+
+router.post('/checkout', async (req, res) => {
+  const cartPrice = getCartPrice()
+
+  const session = await axios.post(
+    `${config.IVY_API_URL}/service/checkout/session/create`,
+    {
+      referenceId: cart.reference,
+      category: '0001',
+      price: {
+        totalNet: cartPrice.subtotalNet,
+        vat: cartPrice.vat,
+        shipping: cartPrice.shipping,
+        total: cartPrice.total,
+      },
+      lineItems: cart.items.map(item => ({
+        name: item.name,
+        singleNet: item.price_net,
+        singleVat: item.price_vat,
+        amount: item.price_total,
+        image: item.image,
+      })),
+    },
+    {
+      Headers: {
+        'X-Ivy-Api-Key': config.IVY_API_KEY,
+      },
+    }
+  )
+
+  console.log('session:', session)
 })
 
 module.exports = router
