534bb9548fdc8a9e1bfb57c4a3858362fb194b96
Lukas Gisder-Dubé <lukas@dube.io>
2022-05-23 16:11:01 +0200

    change ivy api key

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 3300,
   IVY_API_KEY:
-    process.env.IVY_API_KEY || '62569858532fd04f051a03f8.d467d858-a823-4b5a-abc5-3347bb77fb1e',
+    process.env.IVY_API_KEY || '626ac95d96755916a833970c.a21f340e-b12b-48f2-aad9-9e5b6e22499e',
   IVY_CDN_URL: process.env.IVY_CDN_URL || 'https://cdn.dev.getivy.de',
   IVY_API_URL: process.env.IVY_API_URL || 'https://api.dev.getivy.de/api',
 }
