9a118760521c07efb51d57d2556bc12a0f0f6460
idai-ivy <102014300+idai-ivy@users.noreply.github.com>
2022-07-21 16:39:39 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 3300,
   IVY_API_KEY:
-    process.env.IVY_API_KEY || '62b99223309e975dc94f8efa.66687594-b9e1-466d-9541-5181f91224d5',
+    process.env.IVY_API_KEY || '62d957839cb7930f638d29cf.00b460fa-e53d-4251-b2dc-2e6398fd5b87',
   IVY_CDN_URL: process.env.IVY_CDN_URL || 'https://cdn.dev.getivy.de',
   IVY_API_URL: process.env.IVY_API_URL || 'https://api.dev.getivy.de/api',
 }
