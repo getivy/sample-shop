4f051db8f0e43fd631fcd0f603fdbdca7ac43140
kostoman <805344+kostoman@users.noreply.github.com>
2022-09-02 14:38:53 +0200

    feat: add complete callback example

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,43 @@ const shippingMethods = require('../data/shipping.json')
 const vouchers = require('../data/voucher.json')
 const cart = require('../data/cart.json')
 
+router.post('/callback/complete', (req, res) => {
+  const signature = req.get('X-Ivy-Signature')
+  const data = req.body
+
+  const expectedSignature = sign(data)
+
+  if (signature !== expectedSignature) throw new Error('invalid signature!')
+
+  console.log(data)
+
+  const hasSameItems = cart.items.every(cartItem => {
+    return data.lineItems.find(
+      lineItem =>
+        lineItem.name === cartItem.name &&
+        lineItem.quantity === cartItem.amount &&
+        lineItem.singleNet === cartItem.price_net
+    )
+  })
+
+  const response = {
+    result: hasSameItems ? 'approved' : 'declined',
+    redirectUrl: '',
+  }
+
+  const expectedResponse = sign(response)
+
+  if (!hasSameItems) {
+    res.status(400)
+  }
+
+  res.setHeader('X-Ivy-Signature', expectedResponse)
+  res.json(response)
+})
+
 router.post('/callback/quote', (req, res) => {
   const response = {
-    currency: req.body.currency
+    currency: req.body.currency,
   }
 
   const signature = req.get('X-Ivy-Signature')
@@ -18,12 +52,14 @@ router.post('/callback/quote', (req, res) => {
   const expectedSignature = sign(data)
 
   if (signature !== expectedSignature) throw new Error('invalid signature!')
-  
-  if(req.body.shipping) {
-    response.shippingMethods = shippingMethods.filter(item => !!item.countries.includes(req.body.shipping.shippingAddress.country))
+
+  if (req.body.shipping) {
+    response.shippingMethods = shippingMethods.filter(
+      item => !!item.countries.includes(req.body.shipping.shippingAddress.country)
+    )
   }
 
-  if(req.body.discount) {
+  if (req.body.discount) {
     response.discount = vouchers.find(voucher => voucher.voucher === req.body.discount.voucher)
   }
 
@@ -34,10 +70,6 @@ router.post('/callback/quote', (req, res) => {
 })
 
 router.get('/callback/success', (req, res) => {
-  res.redirect('/success')
-})
-
-router.get('/success', (req, res) => {
   // TODO get order details
 
   res.render('success', {
@@ -88,7 +120,7 @@ router.get('/all-buttons', (req, res) => {
     items: cart.items,
     ...getCartPrice(),
     cdnUrl: config.IVY_CDN_URL,
-    version: process.env.npm_package_version
+    version: process.env.npm_package_version,
   })
 })
 
@@ -98,18 +130,17 @@ router.get('/', (req, res) => {
     items: cart.items,
     ...getCartPrice(),
     cdnUrl: config.IVY_CDN_URL,
-    version: process.env.npm_package_version
+    version: process.env.npm_package_version,
   })
 })
 
 router.post('/checkout', async (req, res) => {
   const cartPrice = getCartPrice()
-
-  const generateReferenceId = (Math.random().toString(36)+'00000000000000000').slice(2, 13)
+  const generateReferenceId = (Math.random().toString(36) + '00000000000000000').slice(2, 13)
 
   try {
     const data = {
-      express: !!req.query.express,
+      express: req.query.express,
       referenceId: generateReferenceId,
       category: '5999',
       price: {
@@ -125,6 +156,7 @@ router.post('/checkout', async (req, res) => {
         singleVat: item.price_vat,
         amount: item.price_total,
         image: item.image,
+        quantity: req.query.broken ? 100 : item.amount,
       })),
       billingAddress: {
         country: 'DE',
@@ -147,13 +179,9 @@ router.post('/checkout', async (req, res) => {
     )
 
     console.log('session:', session)
-    if(!!req.query.tab) {
-      res.json({
-        url: session.redirectUrl
-      })  
-    } else {
-      res.redirect(session.redirectUrl)
-    }
+    res.json({
+      url: session.redirectUrl,
+    })
   } catch (err) {
     console.error(err.response.data)
     res.status(400).send(err.response.data.message)
