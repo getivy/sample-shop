1283cb7d052c1270e45129f27107fa70f7a9d83a
kostoman <805344+kostoman@users.noreply.github.com>
2023-10-12 17:44:28 +0200

    add klarna1 stuff

--- src/routes/index.js
+++ src/routes/index.js
@@ -204,6 +204,14 @@ router.get('/klarna/success', (req, res) => {
   })
 })
 
+router.get('/klarna1/success', (req, res) => {
+  res.render('klarna-success', {
+    title: 'Ivy Demo Store',
+    cdnUrl: config.IVY_CDN_URL,
+    version: process.env.npm_package_version,
+  })
+})
+
 router.get('/dynamic', (req, res) => {
   res.render('dynamic', {
     title: 'Ivy Demo Store',
