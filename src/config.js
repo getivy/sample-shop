ff540f554eba45199c4235a634573b84ab76b224
cdans <ch.dansard@gmail.com>
2024-01-09 20:59:59 +0100

    Update env with instant_refund

--- src/config.js
+++ src/config.js
@@ -4,4 +4,5 @@ module.exports = {
   IVY_CDN_URL: process.env.IVY_CDN_URL,
   IVY_API_URL: process.env.IVY_API_URL,
   IVY_WEBHOOK_SIGNING_SECRET: process.env.IVY_WEBHOOK_SIGNING_SECRET,
+  INSTANT_REFUND: process.env.INSTANT_REFUND === 'true',
 }
